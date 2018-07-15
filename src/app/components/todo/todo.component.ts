e807a7ebd2438a766e0384eceff24ce3e70c44b7
Carlos Martin <cjmar87@gmail.com>
2018-07-14 23:14:49 -0500

    terminamos

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TodoService } from '../../services/todo.service';
+import { element } from 'protractor';
+
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
+  providers: [TodoService]
 })
 export class TodoComponent implements OnInit {
 
-  constructor() { }
+  todoList: any[];
+
+  constructor(private todoService: TodoService) { }
 
   ngOnInit() {
+    this.todoService.getTodoList().snapshotChanges()
+      .subscribe(item => {
+        this.todoList = [];
+        // tslint:disable-next-line:no-shadowed-variable
+        item.forEach(element => {
+          const x = element.payload.toJSON();
+          x['$key'] = element.key;
+          this.todoList.push(x);
+        });
+        this.todoList.sort((a, b) => {
+          return a.isChecked - b.isChecked;
+        });
+      });
+  }
+
+  addTodo(itemTitle) {
+    this.todoService.addTodo(itemTitle.value);
+    itemTitle.value = null;
+
+  }
+
+  updateTodo($key: string, isChecked: boolean) {
+    this.todoService.updateTodo($key, !isChecked);
+  }
+
+  deleteTodo($key: string) {
+    if (confirm('estas seguro de eliminar?')) {
+      this.todoService.removeTodo($key);
+    }
   }
 
 }
